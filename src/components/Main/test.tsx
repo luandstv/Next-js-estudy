8b70fe5b7a1a0c6e6750de4f47854abc8d603992
Luan Alves <luantensa@gmail.com>
2024-08-28 00:26:01 -0300

    configurando jest para testes na aplicação

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    // Renderiza o componente
+    render(<Main />)
+    // Busca o elemento  e verifica a existencia dele
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+  })
+})
