db1165b7a980c9ca45df04a093e5e2587f5b91e3
Luan Alves <luantensa@gmail.com>
2024-08-27 22:54:08 -0300

    configurando EsLint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import Script from "next/script";
 
 export default function Home() {
+  const foo = "sla";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
