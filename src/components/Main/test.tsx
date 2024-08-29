67fb557d3b96fa6fb38b165af88695c3fe61ce9c
Luan Alves <luantensa@gmail.com>
2024-08-28 21:46:49 -0300

    configurando jest para testes na aplicação

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,10 +4,13 @@ import Main from '.'
 describe('<Main />', () => {
   it('should render the heading', () => {
     // Renderiza o componente
-    render(<Main />)
+    const { container } = render(<Main />)
     // Busca o elemento  e verifica a existencia dele
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+
+    // gerar snapshot
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
